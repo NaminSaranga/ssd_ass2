f8d07b4d1badc1e52058610de4d1f24a2df7e957
Namin Saranga <naminsaranga54@gmail.com>
2021-10-03 12:39:09 +0530

    GoogleDriveOAuth

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    var clientId ="511933029907-vr4j464376rilgnq5jnaq6kbjfqf1ian.apps.googleusercontent.com" 
+
+    var redirect_uri="http://localhost/droogle/driveupload.html"
+
+    var scope="https://www.googleapis.com/auth/drive"
+
+    var url=""
+
+
+    $("#login").click(function(){
+
+        signIn(clientId,redirect_uri,scope,url);
+    
+    
+    })
+
+
+    function signIn(clientId,redirect_uri,scope,url){
+
+        url = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri="+redirect_uri
+        +"&prompt=consent&response_type=code&client_id="+clientId+"&scope="+scope
+        +"&access_type=offline";
+
+        window.location = url;
+        
+    }
+
+
+});
